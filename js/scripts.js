31af74a482f4ae2557d538bde5f24ae40d9f6409
wentdavid <davidwent@me.com>
2022-10-07 15:39:23 +0200

    3(i). convert vanilla js to jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -24,11 +24,10 @@ function newItem(){
     });
     
      //3(i). Adding the delete button "X": 
-       let crossOutButton = document.createElement("crossOutButton");
-         crossOutButton.appendChild(document.createTextNode("X"));
-         li.appendChild(crossOutButton);
-    
-         crossOutButton.addEventListener("click", deleteListItem);
+     let crossOutButton = $('<crossOutButton></crossOutButton>');
+     crossOutButton.append(document.createTextNode('X'));
+     li.append(crossOutButton);
+     
      //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
        function deleteListItem(){
              li.classList.add("delete")
